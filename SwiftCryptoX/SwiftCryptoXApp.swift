1e21c6bff15b380e18e086fb067bca1181806a9c
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-08-21 14:17:11 -0400

    Initial Commit

---
+++ SwiftCryptoX/SwiftCryptoXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoXApp.swift
+//  SwiftCryptoX
+//
+//  Created by Jon Rosenblum on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
