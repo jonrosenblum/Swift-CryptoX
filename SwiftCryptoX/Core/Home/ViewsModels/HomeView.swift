a5f13c1a83402bc4034248df64f3d25b033a8b4b
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-08-21 15:05:51 -0400

    Testing commit

---
+++ SwiftCryptoX/Core/Home/ViewsModels/HomeView.swift
@@ -0,0 +1,46 @@
+//
+//  HomeView.swift
+//  SwiftCryptoX
+//
+//  Created by Jon Rosenblum on 8/21/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack{
+                HStack{
+                    CircleButtonView(iconName: "info")
+                    Spacer()
+                    Text("Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                }
+                .padding(.horizontal)
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
